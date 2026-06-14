2868406822fa4d9dbc11c5ab42048e77e5292ca9
vivekchaudhari14 <vivekchaudhari14@zohomail.in>
2026-06-14 17:55:20 +0530

    Insert Operation Working Now

---
+++ EmpManagement/src/main/java/com/spring/management/dao/EmployeeDao.java
@@ -0,0 +1,7 @@
+package com.spring.management.dao;
+
+import com.spring.management.entities.Employee;
+
+public interface EmployeeDao {
+	public int insert(Employee employee);
+}
---
+++ EmpManagement/src/main/java/com/spring/management/dao/EmployeeDaoImple.java
@@ -0,0 +1,34 @@
+package com.spring.management.dao;
+
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.management.entities.Employee;
+
+public class EmployeeDaoImple implements EmployeeDao {
+
+	private HibernateTemplate hibernateTemplate;
+	
+	
+	
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+
+
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
+
+
+
+	@Override
+	@Transactional
+	public int insert(Employee employee) {
+		
+		Integer i = (Integer)this.hibernateTemplate.save(employee);
+		return i;
+	}
+	
+}
